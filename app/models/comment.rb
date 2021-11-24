8005d5871b54e336fad0c37981b8b6829656cc08
bcharlotin1 <biancacharlotin1@gmail.com>
2021-11-24 17:00:00 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,9 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
 
+  # accepts_nested_attributes_for :posts, reject_if: :reject_posts
+
+  # def reject_posts(attributes)
+  #   attributes['title'].blank?
+  # end
 end
